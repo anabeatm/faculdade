5ae5daddb54c21a6c57f1fe5ab9de1ebd77367d1
Ana Beatriz Tavares Malaquias <ana.beat20051@gmail.com>
2025-06-06 21:57:01 -0300

    Create 06-06.java

---
+++ 2025/AGII/2tri/06-06.java
@@ -0,0 +1,43 @@
+// LISTA ENCADEADA/LINKED LIST
+// NÃO HÁ LIMITAÇÃO DE ELEMENTOS
+public class linkedList{
+  No head;
+  
+  public void adicionarInicio(int valor) {
+    No novoElemento = new No(valor);
+    if(head == null) {head = novoElemento;}
+    else {
+      novoElemento.proximoValor = head;
+      head = novoElemento;
+    }
+
+
+    public void imprimir() {
+      No aux = head;
+      while(aux != null) {
+        System.out.print(aux.valor + " -> ");
+        aux = aux.proximoValor;
+      }
+    }
+    System.out.print("null");
+    System.out.println();
+  }
+  
+  private class No {
+    int valor;
+    No proximoValor;
+
+    public No(int valor) {
+      this.valor = valor;
+      this.proximoValor = null;
+    }
+  }
+  
+  public static void main(String[] args) {
+    linkedList lista = new linkedList();
+    lista.adicionarInicio(10);
+    lista.adicionarInicio(5);
+    lista.imprimir();
+  }
+  
+}
