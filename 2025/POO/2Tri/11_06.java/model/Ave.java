95af27e133e801f4004d7b579765470463b9b0df
Ana Beatriz Tavares Malaquias <ana.beat20051@gmail.com>
2025-06-11 20:18:50 -0300

    Create Ave.java

---
+++ 2025/POO/2Tri/11_06.java/model/Ave.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Ave extend Animal {
+  private double envergadura;
+  private String formatoBico;
+  private String corOvo;
+  private boolean voa;
+  
+  public Ave(int idade, double peso. String nomePopular, double envergadura, String formatoBico, String corOvo, boolean voa) {
+    super(idade, peso, nomePopular); // --> comando para chamar construtores de classes superiores
+    this.evergadura = envergadura;
+    this.formatoBico = formatoBico;
+    this.corOvo = corOvo;
+    this.voa = voa;
+  } // necessário existir um construtor padrão na classe mãe ou NENHUM construtor; sem um construtor padrão é possível utilizar o super
+}
