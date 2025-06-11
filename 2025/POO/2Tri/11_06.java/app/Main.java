251d1b22c11f5ba0305df4f434bca4e402a9a49e
Ana Beatriz Tavares Malaquias <ana.beat20051@gmail.com>
2025-06-11 20:21:53 -0300

    Update Main.java

--- 2025/POO/2Tri/11_06.java/app/Main.java
+++ 2025/POO/2Tri/11_06.java/app/Main.java
@@ -1,1 +1,13 @@
 
+package app;
+
+import model.Animal;
+import model.Ave;
+
+public class Main {
+  public static void main(String[] args) {
+    Ave queroQuero = new Ave(1, .100, "Quero quero", .50, "reto", "branco", true);
+
+    queroQuero.emitirSom()
+  }
+}
