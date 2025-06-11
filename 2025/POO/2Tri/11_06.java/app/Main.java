b4d51ade759ecf864bb13e83cc4c3a83ce65ec37
Ana Beatriz Tavares Malaquias <ana.beat20051@gmail.com>
2025-06-11 20:31:51 -0300

    Update Main.java

--- 2025/POO/2Tri/11_06.java/app/Main.java
+++ 2025/POO/2Tri/11_06.java/app/Main.java
@@ -8,6 +8,14 @@ public class Main {
   public static void main(String[] args) {
     Ave queroQuero = new Ave(1, .100, "Quero quero", .50, "reto", "branco", true);
 
-    queroQuero.emitirSom()
+    queroQuero.emitirSom(); // polimorfismo --> capacidade de objetos de diferentes classes responder a mesma mensagem (chamada a um método) de maneira diferente, dependendo da sua classe
+
+    Animal queroQuero = new Ave(1, .100, "Quero quero", .50, "reto", "branco", true);
+    queroQuero.emitirSom();
+    // saída: pia
+
+    List<Animal> listaAnimal = new ArrayList<Animal>(20);
+    listaAnimal.add(queroQuero);
+    
   }
 }
