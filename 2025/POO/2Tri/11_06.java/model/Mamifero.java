7580eac6687e56f3473d3f6074d88b5366324e5d
Ana Beatriz Tavares Malaquias <ana.beat20051@gmail.com>
2025-06-11 20:36:55 -0300

    Update Mamifero.java

--- 2025/POO/2Tri/11_06.java/model/Mamifero.java
+++ 2025/POO/2Tri/11_06.java/model/Mamifero.java
@@ -3,6 +3,19 @@ package model;
 public class Mamifero extend Animal {
   private boolean temPelo;
   private String rabo;
+  private String som;
 
-  public Mamifero ()
+  public Mamifero (int idade, double peso, String nomePopular, boolean temPelo, String rabo, String som) {
+    this.idade = idade;
+    this.peso = peso;
+    this.nomePopular = nomePopular;
+    this.temPelo = temPelo;
+    this.rabo = rabo;
+    this.som = som;
+  }
+
+ // @Override
+   /// public void emitirSom() {
+     // System.out.println("")
+   // }
 }
