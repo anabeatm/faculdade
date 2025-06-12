4baeb092c9eac2fc91f2ef931ec230303e238b9a
Ana Beatriz Tavares Malaquias <ana.beat20051@gmail.com>
2025-06-11 21:27:30 -0300

    Update Animal.java

--- 2025/POO/2Tri/11_06.java/model/Animal.java
+++ 2025/POO/2Tri/11_06.java/model/Animal.java
@@ -1,7 +1,7 @@
 // HERANÇA --> é um conceito fundamental que permite que uma classe (classe derivada) herde atributos e métodos de outra classe (classe base)
 package model;
 
-public class Animal {
+public abastract class Animal { // classe concreta: posso criar instancia; classe abstract: genérico, conceitual, não posso ter implementações especifícas --> n cria instancias
   protected int idade;
   protected double peso;
   protected String nomePopular;
@@ -15,4 +15,6 @@ public class Animal {
   public void emitirSom() {
     System.out.println("som não definido")
   }
+
+  public abstract void locomocao();
 }
